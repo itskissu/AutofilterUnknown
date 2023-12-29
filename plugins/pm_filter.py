7630dea02aac560293e8ead1d07ef9378e8f13f6
No one knows <142857872+unknownbuddy1@users.noreply.github.com>
2023-12-29 14:11:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1186,7 +1186,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('Aʙᴏᴜᴛ', callback_data='about')
 
                 ],[                                        
-                    InlineKeyboardButton('Aɴʏ Qᴜᴇʀʏ', url="t.me/Unknown_Support_Bot")                            
+                    InlineKeyboardButton('Aɴʏ Qᴜᴇʀʏ', url="t.me/KissuMayIHelpYouBot")                            
                 ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
