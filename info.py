2948721070ec3b819f02e377826c40ea3cdf2fc6
No one knows <142857872+unknownbuddy1@users.noreply.github.com>
2023-12-29 13:18:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -92,11 +92,11 @@ if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
 else:
-    ON_HEROKU = False
+    ON_HEROKU = True
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://autofilterstream-rohaniscoder.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://autofilterstream-rohaniscoder.koyeb.app/".format(FQDN, PORT)
+URL = "https://kissu123456-a76b42283d73.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://kissu123456-a76b42283d73.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -108,12 +108,12 @@ if 'DYNO' in environ:
     APP_NAME = str(getenv('APP_NAME'))
 
 else:
-    ON_HEROKU = False
+    ON_HEROKU = True
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://autofilterstream-rohaniscoder.koyeb.app/".format(FQDN)
+    URL = "https://kissu123456-a76b42283d73.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://autofilterstream-rohaniscoder.koyeb.app/".format(FQDN)
+    URL = "https://kissu123456-a76b42283d73.herokuapp.com/".format(FQDN)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
